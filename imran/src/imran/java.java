997c89a7a8c207f210f9b1567e69ef5d05d2fb36
Imran <Imran@DESKTOP-KIN7N7I>
2022-01-31 15:43:18 +0530

    my first commit

---
+++ imran/src/imran/java.java
@@ -0,0 +1,10 @@
+package imran;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to programming in java..");
+	}
+
+}
