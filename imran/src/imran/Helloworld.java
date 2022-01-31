6cc99a9d34b3b3a3793bebefa840c6b3375ffdcd
Imran <Imran@DESKTOP-KIN7N7I>
2022-01-31 15:57:01 +0530

    my first commit

---
+++ imran/src/imran/Helloworld.java
@@ -0,0 +1,10 @@
+package imran;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("wlcome to programming in java.");
+	}
+
+}
